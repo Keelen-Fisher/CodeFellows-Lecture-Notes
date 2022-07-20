2c1b971d4a664419a6d8d753b0c7fd581b8b37bc
Keelen Fisher <keelencarrera@gmail.com>
2022-07-19 22:36:06 -0500

    added object example code and class07

--- CSS&HTMLPractice/practive.js
+++ CSS&HTMLPractice/practive.js
@@ -22,9 +22,119 @@
 let kittenSection = document.getElementById()
 console.dir(kittenSection);
 
+// ************ CONSTRUCTOR REFACTOR ***********
+let kittenCaboodle = [];
+
+function Kitten(name, interests, isGoodWithCats, isGoodWithDogs, isGoodWithKids, photo){
+  this.name = name;
+  this.interests = interests;
+  this.isGoodWithCats = isGoodWithCats;
+  this.isGoodWithDogs = isGoodWithDogs;
+  this.isGoodWithKids = isGoodWithKids;
+  this.photo = photo;
+  this.age = 0;
+
+  kittenCaboodle.push(this);
+
+}
+// unique to each store - city, minCust, maxCust, avgCookie
+
+
+// ************** PROTOTYPE METHODS *************
+
+Kitten.prototype.getAge = function(){
+  this.age = `${randomAge(3,12)} months`;
+};
+
+
+
+
+Kitten.prototype.render = function(){
+  //  STEP 2: CREATE THE ELEMENT
+  let articleElem = document.createElement('article')
+  // STEP 3 - optional, give it context
+  // STEP 4 - ADD IT TO THE HTML DOCUMENT 
+  // parent.appendChild(child to append);
+  kittenSection.appendChild(articleElem);
+
+  // create h2 element - post the kitten's name
+  let h2Elem = document.createElement('h2');
+  // context - kitten's name 
+
+  h2Elem.textContent = this.name; 
+  articleElem.appendChild(h2Elem);
+
+  let pElem = document.createElement('p');
+  pElem.textContent = `${this.name} is ${this.age}`;
+  articleElem.appendChild(pElem);
+
+  let ulElem = document.createElement('ul');
+  articleElem.appendChild(ulElem);
+
+  for(let i = 0; i< this.interests.length; i++){
+    let liElem = createElement('li');
+    liElem.textContent = this.interests[i];
+    ulElem.appendChild(liElem);
+//  you can also do append(h2Elem, pElem, ulElem)
+  };
+
+  let imgElem = document.createElement('img');
+  imgElem.src = this.photo;
+  imgElem.alt = `${this.name} is ${this.age}`;
+  articleElem.appendChild(imgElem);
+  
+  // ************ TABLE DOM RENDERING *************
+  let tableElem = document.createElement('table');
+  articleElem.appendChild(tableElem);
+
+  // Now we need the rows <tr></tr>:
+  let row1 = document.createElement('tr');
+  tableElem.appendChild(row1);
+  let th1Elem = document.createElement('th');
+  th1Elem.textContent = 'Good With Cats!';
+  row1.appendChild(th1Elem);
+
+
+  let th2Elem = document.createElement('th');
+  th2Elem.textContent = 'Good With Dogs!';
+  row1.appendChild(th2Elem);
+
+
+  let th3Elem = document.createElement('th');
+  th1Elem.textContent = 'Good With Kids!';
+  row1.appendChild(th3Elem);
+
+  
+}
+
+
+//  **************** Create Kittens with my Constructor********************
+
+let frankie = new Kitten('Frankie', ['wet food', 'fish toy', 'cat nip'],
+true, true, true, 'img/frankie.jpeg');
+
+let jumper = new Kitten('Jumper', ['dry food', 'mouse toy', 'treats'],
+true, false, false, 'img/jumper.jpeg');
+
+
+frankie.getAge();
+frankie.render();
+jumper.getAge();
+jumper.render();
+// and so forth, OR:
+
+function renderKittens(){
+  for(let i = 0; i < kittenCaboodle.length; i++){
+    let currentKitten = kittenCaboodle[i];
+    currentKitten.getAge();
+    currentKitten.render();
+  }
+}
 //  *********** OBJECT LITERALS*****************
+
+// 
 let frankie = {
-  name: 'Frankie',
+  name: 'Frankie', 
   age: 0,
   interests: ['wet food', 'fish toy', 'cat nip'],
   isGoodWithCats: true,
--- class07.md
+++ class07.md
@@ -4,27 +4,29 @@
 
 - Why do we need it?
 
->> Domain modeling is the proces of creating conceptual model in code for a specific problem. A model describes the various entities their attributes and behaviors, as well as the constrints that govern the problem domain.
+>> Domain modeling is the process of creating conceptual model in code for a specific problem. A model describes the various entities their attributes and behaviors, as well as the constraints that govern the problem domain.
 
 - As a communication tool, it defines a vocabulary that can be used within and between both technical and business teams.
 
 ## HTML Table Basics
 
-- What is a table? A structured set of data made uo of rows and columns, and can allow you to quickly and easily look up values that indicate some kind of connnection between different types of data.
+- What is a table? A structured set of data made up of rows and columns, and can allow you to quickly and easily look up values that indicate some kind of connection between different types of data.
 
-- Tables ***SHOULD NOT*** be used for page layouts because it is rather prefered to [CSS layout techniques](https://developer.mozilla.org/en-US/docs/Learn/CSS/CSS_layout). Also, layout tables can reduce accessibility for visually impaired users, and they are not repsonsive.
+- Tables ***SHOULD NOT*** be used for page layouts because it is rather preferred to [CSS layout techniques](https://developer.mozilla.org/en-US/docs/Learn/CSS/CSS_layout). Also, layout tables can reduce accessibility for visually impaired users, and they are not responsive.
 
 - There are 3 different semantic HTML elements used in an HTML table:
   
   - tr: Table Row Element defines the row of cells in a table.
 
-  - td: Table Data Cell Element difines a cell of a table that contains data.
+  - td: Table Data Cell Element defines a cell of a table that contains data.
 
   - th: Table Header Element defines a cell as header of group of table cells.
 
+  - Wanna see a page with a table mainly?? You'll find those types back in the days. Here's a site that's still functional after 20 years: [Click Here:](https://www.spacejam.com/1996/)
+
 ## Introducing Constructors
 
-- A constructor is a fubction called using new keywords. As you call a constructor, it will create a new object, run the code in the constructor, return the new object.
+- A constructor is a function called using new keywords. As you call a constructor, it will create a new object, run the code in the constructor, return the new object.
 
 - The term *this* when used with an object literal and constructor are as follows:
 
